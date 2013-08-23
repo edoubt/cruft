12c0481cfebccb4910de8aa5aa60508a304d6001
Ernst Doubt <edoubt@organicmeat.net>
2013-08-22 21:49:39 -0400

    learning clojure by using frak to generate regexes

---
+++ clojure/first_lein/testbed/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to testbed
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clojure/first_lein/testbed/test/testbed/core_test.clj
@@ -0,0 +1,7 @@
+(ns testbed.core-test
+  (:use clojure.test
+        testbed.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
